a237ba01dd3c710092f08f46fa89ef107dd5629e
Kevin Wong <kevinwong6175@gmail.com>
2017-08-08 01:09:02 +0000

    User Story 2 - Sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # devise_scope :user do
-  #   get "/sign_in" => "devise/sessions#new"
-  #   get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
-  # end
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
+  end
   
   devise_for :users
   
